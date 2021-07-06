ca6d668653311c982745720ba4d8fb0d16e93d08
dengyuying <1187232061@qq.com>
2021-07-06 17:20:39 +0800

    框架搭建

---
+++ blog/pages/_app.js
@@ -0,0 +1,15 @@
+/* import '../styles/globals.css'
+import 'antd/dist/antd.css'
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp */
+
+import App from 'next/app'
+
+import 'antd/dist/antd.css'
+
+export default App
