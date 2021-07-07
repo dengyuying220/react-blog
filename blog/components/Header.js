24771a5300e94a9421551121671078f93fbc1f3c
dengyuying <1187232061@qq.com.com>
2021-07-07 22:27:23 +0800

    header, left-right layout, List-1

--- blog/components/Header.js
+++ blog/components/Header.js
@@ -1,35 +1,42 @@
 import React from 'react'
 import style from './header.module.scss'
+import {
+    HomeOutlined,
+    YoutubeOutlined,
+    SmileOutlined,
+  } from '@ant-design/icons';
 
-// import '../s header.module.css'
+// import './header.module.scss'
 
 
-import {Row, Col, Menu, Icon} from 'antd'
-const Header = () => (
-  <div className={style.header}>
-    <Row type="flex" justify="center">
-    <Col  xs={24} sm={24} md={10} lg={10} xl={10}>
-            <span className={style.header_logo}>技术胖</span>
-            <span className={style.header_txt}>专注前端开发,每年100集免费视频。</span>
-        </Col>
+import {Row, Col, Menu} from 'antd'
+const Header = () => {
+    return (
+        <div className={style.header}>
+            <Row type="flex" justify="center">
+                <Col  xs={24} sm={24} md={10} lg={15} xl={12}>
+                    <span className={style.header_logo}>deng</span>
+                    <span className={style.header_txt}>记录前端开发日常。</span>
+                </Col>
+                <Col  xs={0} sm={0} md={14} lg={8} xl={6}>
+                    <Menu mode="horizontal">
+                        <Menu.Item key="home">
+                            <HomeOutlined />
+                            首页
+                        </Menu.Item>
+                        <Menu.Item key="video">
+                            <YoutubeOutlined />
+                            视频
+                        </Menu.Item>
+                        <Menu.Item key="life">
+                            <SmileOutlined />
+                            生活
+                        </Menu.Item>
+                    </Menu>
+                </Col>
+            </Row>
+        </div>
+    )
+}
 
-        <Col className={style.memu_div} xs={0} sm={0} md={14} lg={8} xl={6}>
-            <Menu  mode="horizontal">
-                <Menu.Item key="home">
-                    <Icon type="home" />
-                    首页
-                </Menu.Item>
-                <Menu.Item key="video">
-                    <Icon type="youtube" />
-                    视频
-                </Menu.Item>
-                <Menu.Item key="life">
-                    <Icon type="smile" />
-                    生活
-                </Menu.Item>
-            </Menu>
-        </Col>
-    </Row>
-  </div>
-)
 export default Header;
