e1ec54f47717c6e4cfc8f00469555e35110d5ebe
dengyuying <1187232061@qq.com>
2021-07-19 19:28:27 +0800

    index-list-api

--- blog/service/app/controller/default/home.js
+++ blog/service/app/controller/default/home.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
@@ -9,13 +9,13 @@ class HomeController extends Controller {
     this.ctx.body=result
   }
   async getArticleList(){
-    let sql = 'SELECT article.id as id,'+
-              'article.title as title,'+
-              'article.introduce as introduce,'+
-              'article.add_time as add_time,'+
-              'article.view_count as view_count ,'+
-              '.type.type_name as type_name '+
-              'FROM article LEFT JOIN type ON article.type_id = type.Id'
+    let sql = "SELECT article.id as id,"+
+              "article.title as title,"+
+              "article.introduce as introduce,"+
+              "FROM_UNIXTIME(article.add_time, '%Y-%m-%d') as add_time,"+
+              "article.view_count as view_count ,"+
+              ".type.type_name as type_name "+
+              "FROM article LEFT JOIN type ON article.type_id = type.Id"
  
     const results = await this.app.mysql.query(sql)
 
